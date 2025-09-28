4e805c4e7fd89c5cecafb52c968a28a800c96bfd
Myz09 <160198749+Myz09@users.noreply.github.com>
2025-09-28 09:02:00 -0400

    9am fallgame works

---
+++ Scripts/basket_catch.gd
@@ -0,0 +1,26 @@
+extends Node2D
+@onready var pause_menu = $Pausemenu
+@onready var score = $Score
+@onready var health =$Health
+var paused = false;
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	score.text="Score: " +  str(Globals.fallScore);
+	health.text="Health: " +  str(Globals.fallhealth);
+	if (Globals.fallhealth<=0):
+		get_tree().change_scene_to_file("res://cut_screen.tscn")
+	
+		
+#func pauseMenu():
+	#if paused:
+		#pause_menu.hide()
+		#Engine.time_scale=1
+	#else:
+		#pause_menu.show()
+		#Engine.time_scale=0;
+	#paused = !paused
